1941a8520d441c1c31d5149266ffe9f06a300e89
QodirovDoston <qodirovdoston806@gmail.com>
2023-01-16 18:17:32 +0500

    dost

--- src/conpenants/Footer/Footer.jsx
+++ src/conpenants/Footer/Footer.jsx
@@ -7,53 +7,87 @@ const Footer = (props) => {
     <>
   <div className="container">
     <div className="row justify-content-around mt-5">
-        <div className="col-2">
+        <div className="col-3"> 
             <img src={icon} alt="" />
           <ul className='list_ati'>
-          <i class="fa-light fa-location-dot"></i>
+          <div className="addres">
+          <i class="fa-sharp fa-solid fa-location-dot"></i>
             <li className='serbiya'>Dhaka, Bangladesh</li>
+          </div>
+          <div className="addres">
+          <i class="fa-solid fa-phone-volume"></i>
             <li className='serbiya'>0943833399</li>
+          </div>
+          <div className="addres">
+          <i class="fa-solid fa-envelope"></i>
             <li className='serbiya'>support@efood.com</li>
+          </div>
           </ul>
-          <img src="" alt="" />
+          <div className="callsenter">
+            <div className="defo">
+          <i class="fa-brands fa-facebook"></i>
+            </div>
+            <div className="defo">
+          <i class="fa-brands fa-twitter"></i>
+            </div>
+            <div className="defo">
+          <i class="fa-brands fa-square-instagram"></i>
+            </div>
+            <div className="defo">
+          <i class="fa-sharp fa-solid fa-paper-plane"></i>
+            </div>
+          </div>
         </div>
         <div className="col-2">
-            {/* <img src={icon} alt="" /> */}
           <ul className='list_ati'>
             <li className='serbiya'>Service</li>
-            <li className='serbiya'>How it works</li>
-            <li className='serbiya'>Home delivery</li>
-            <li className='serbiya'>Prducts</li>
-            <li className='serbiya'>Menu</li>
+            <li className='serbiya1'>How it works</li>
+            <li className='serbiya1'>Home delivery</li>
+            <li className='serbiya1'>Prducts</li>
+            <li className='serbiya1'>Menu</li>
           </ul>
         </div>
         <div className="col-2">
           <ul className='list_ati'>
             <li className='serbiya'>Company</li>
-            <li className='serbiya'>About Us</li>
-            <li className='serbiya'>News</li>
-            <li className='serbiya'>Our trusted partner</li>
-            <li className='serbiya'>New users FAQ</li>
+            <li className='serbiya1'>About Us</li>
+            <li className='serbiya1'>News</li>
+            <li className='serbiya1'>Our trusted partner</li>
+            <li className='serbiya1'>New users FAQ</li>
           </ul>
         </div>
         <div className="col-2">
           <ul className='list_ati'>
             <li className='serbiya'> Supports</li>
-            <li className='serbiya'>Help center</li>
-            <li className='serbiya'>Feedbcak</li>
-            <li className='serbiya'>Contact us</li>
-            <li className='serbiya'>Terms conditins</li>
+            <li className='serbiya1'>Help center</li>
+            <li className='serbiya1'>Feedbcak</li>
+            <li className='serbiya1'>Contact us</li>
+            <li className='serbiya1'>Terms conditins</li>
           </ul>
         </div>
         <div className="col-2">
           <ul className='list_ati'>
             <li className='serbiya'>Resources</li>
-            <li className='serbiya'>Download app</li>
-            <li className='serbiya'>Blog</li>
-            <li className='serbiya'>What’s new</li>
-            <li className='serbiya'>Sitemap</li>
+            <li className='serbiya1'>Download app</li>
+            <li className='serbiya1'>Blog</li>
+            <li className='serbiya1'>What’s new</li>
+            <li className='serbiya1'>Sitemap</li>
           </ul>
         </div>
+    </div>
+  </div>
+  <hr />
+  <div className="container">
+    <div className="row justify-content-between mt-5">
+      <div className="col-5">
+        <p className='privade'>© 2021 EFood - All rights reserved.</p>
+      </div>
+      <div className="col-3 d-flex justify-content-between">
+        <p className='privade'>Privacy</p>
+        <p className='privade'>Security</p>
+        <p className='privade'>Terms</p>
+
+      </div>
     </div>
   </div>
     </>
