2cbd1866dc674d92b4f02a1995b05673d7d4f66e
QodirovDoston <qodirovdoston806@gmail.com>
2023-01-19 10:48:11 +0500

    dfdf

--- src/conpenants/ANavbar/Navbar.jsx
+++ src/conpenants/ANavbar/Navbar.jsx
@@ -9,7 +9,7 @@ const search = require("../../img/cart.png")
     <>
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
+    <a href='#!' class="navbar-brand">Navbar</a>
     <form class="d-flex">
       {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> */}
       <button class="btn btn-outline-success" type="submit">Search</button>
